e19aa074875f061abba5f2fe77c1140795568f03
Shrikanth1997 <shrikanthrajan@gmail.com>
2021-06-23 23:43:04 -0400

    Added Simulated Annealing method
     - Added Greedy initial solution
     - Added neighbor searching function

--- subset_sum.cpp
+++ subset_sum.cpp
@@ -4,6 +4,7 @@
 #include<numeric>
 #include<algorithm>
 #include<fstream>
+#include<math.h>
 #include<string>
 
 using namespace std;
@@ -16,18 +17,18 @@ typedef struct inp {
 }inp; 
 
 // Number of Instances needed to be tested
-int totalInstances = 100;
+int totalInstances = 10;
 
 // Timer variables
 // Timer for running each benchmark instance
 using chrono::high_resolution_clock;
 using chrono::duration_cast;
 using chrono::duration;
-using chrono::seconds;
+using chrono::microseconds;
 
 auto t1 = high_resolution_clock::now();
 auto t2 = high_resolution_clock::now();
-auto t_ms = duration_cast<seconds>(t2 - t1);
+auto t_ms = duration_cast<microseconds>(t2 - t1);
 
 // Time Limit for each instance
 int timeLimit = 60;
@@ -35,15 +36,15 @@ int timeLimit = 60;
 // Generate the random inputs required for each instance
 vector<inp> generateInput(vector<inp>inputBenchmark) {
 
-	int length = 35;
+	int length = 30;
 	for(int i=0;i<totalInstances;i++){
 		
 		for(int j=0;j<length;j++){
-			inputBenchmark[i].arr.push_back(rand()%100);
+			inputBenchmark[i].arr.push_back(rand()%100 + 1);
 		}
 		inputBenchmark[i].n = length;
 		auto rand_sum = accumulate(inputBenchmark[i].arr.begin(), inputBenchmark[i].arr.end(), 0);
-		inputBenchmark[i].sum = rand() % rand_sum;
+		inputBenchmark[i].sum = rand() % rand_sum + 1;
 
 		length+=2;
 	}	
@@ -64,7 +65,7 @@ bool isSubsetSum(vector<int> arr, int n, int sum)
 			t2 = high_resolution_clock::now();
 
     	/* Getting number of milliseconds as an integer. */
-    	t_ms = duration_cast<seconds>(t2 - t1);
+    	t_ms = duration_cast<microseconds>(t2 - t1);
 		
 			// If the benchmark instance took more than 60s then time limit exceeded
 			if(t_ms.count() > timeLimit){
@@ -80,22 +81,30 @@ bool isSubsetSum(vector<int> arr, int n, int sum)
 
 bool comp (int i,int j) { return (i>j); }
 
-bool subsetGreedy(vector<int> arr, int n, int sum){
-	
-	sort(arr.begin(), arr.end(), comp);
+
+bool subsetGreedy(vector<int> arr, int n, int sum, vector<int> &setGreedy){
+
+
+	sort(arr.begin(), arr.end(), comp);	
 
 	int s=0;
 
 	for(int i=0;i<n;i++){
-		if(s+arr[i]<=sum)
+		if(s+arr[i]<=sum){
 			s+=arr[i];
+			setGreedy.push_back(arr[i]);	
+		}	
 	}
 
-	for(int i=0;i<n;i++)
+	/*for(int i=0;i<n;i++)
 		cout<<arr[i]<<" ";
 	cout<<endl;	
 	cout<<"Output for greedy: "<<s<<" ,Actual sum: "<<sum<<" with difference: "<<sum-s<<"\n\n";
-
+	cout<<"Set for Greedy = "<<endl;
+	for(int i=0;i<setGreedy.size();i++)
+		cout<<setGreedy[i]<<" ";
+	cout<<endl<<endl;
+	*/
 
 	if(sum == s)
 		return true;
@@ -147,6 +156,94 @@ param TargetSum := 32;*/
 	
 }
 
+
+void printVector(vector<int> arr){
+	for(int i=0;i<arr.size();i++)
+		cout<<arr[i]<<" ";
+	cout<<endl;
+}
+
+vector<int> getNeighbor(vector<int> A, vector<int> set){
+	//srand(time(NULL));
+
+	vector<int> B = A;	
+	int i = rand() % set.size();
+	int j = rand() % set.size();
+	
+	auto xi = find(A.begin(), A.end(), set[i]);
+	if(xi != A.end()){
+		auto bxi = find(B.begin(), B.end(), set[i]);
+		if(bxi != B.end()){
+			int pos = bxi - B.begin(); 
+			B.erase(B.begin() + pos);
+		}
+	}
+	else
+		B.push_back(set[i]);
+	
+	auto xj = find(A.begin(), A.end(), set[j]);
+	float r = static_cast <float> (rand())/static_cast <float> (RAND_MAX);
+	if(xj != A.end()){
+		if( r < 0.5 ){
+			auto bxj = find(B.begin(), B.end(), set[j]);
+			if(bxj != B.end()){
+				int pos = bxj - B.begin();
+				B.erase(B.begin() + pos);
+			}
+		}
+	}
+	else {
+		if( r < 0.5 ) {
+			B.push_back(set[j]);
+		}
+	}
+
+	return B;
+}
+
+bool simulatedAnnealing(vector<int> ini, int actual_sum, vector<int> set){
+	
+	float temp = 5000; //Change back to 5000
+	float alpha = 0.983; //Change back to 0.983
+	float epsilon = 0.001;
+	
+	int itr = 0;
+	int initial_sum = accumulate(ini.begin(), ini.end(), 0);
+	int nsum;
+
+	cout<<"Expected: "<<actual_sum<<"\n";
+	while(temp > epsilon) {
+		while(itr < 10000){ // Change back to itr<1000
+			//srand(time(NULL));	
+			vector<int> neighbor = getNeighbor(ini, set);
+			auto neighbor_sum = accumulate(neighbor.begin(), neighbor.end(), 0);
+		
+			// Exit if the solution is found
+			if(neighbor_sum == actual_sum){
+				cout<<"Found true\n";
+				return true;
+			}
+			
+			float delta = neighbor_sum - initial_sum;
+			
+			float r = static_cast <float> (rand())/static_cast <float> (RAND_MAX);
+			if(delta < 0)
+				ini = neighbor;
+			else if(r < exp(-1* (delta/temp)))
+				ini = neighbor; 
+			itr++;
+			nsum = neighbor_sum;
+		}
+		temp *= alpha;
+	}
+
+		
+	cout<<"SA sum: "<<nsum<<"\n";
+	cout<<"Difference: "<<nsum - actual_sum<<"\n";
+	return false;
+}
+
+
 int main(){
 	srand(time(NULL));	
 
@@ -159,6 +256,7 @@ int main(){
 		
 		int foundSetE = 0, notFoundSetE = 0, benchmarksCompleteE = 0;
 		int foundSetG = 0, notFoundSetG = 0, benchmarksCompleteG = 0;
+		int foundSetS = 0, notFoundSetS = 0, benchmarksCompleteS = 0;
 
 	/*
 		cout<<"Running Exhaustive Algorithm------------------------------------"<<endl;
@@ -197,27 +295,31 @@ int main(){
 		cout<<"Fraction of benchmarks completed: " << benchmarksCompleteE << " out of " << totalInstances 
 																							 << " Percentage: " << (benchmarksCompleteE*100)/totalInstances <<endl;
 		*/
-		cout<<endl<<"Running Greedy Algorithm----------------------------------------"<<endl;
+	
+	/*
+	cout<<endl<<"Running Greedy Algorithm----------------------------------------"<<endl;
 
 
 		// Run each instance
 	for(int i=0;i<totalInstances;i++){
 	
     	t1 = high_resolution_clock::now();
+	
 
+	vector<int> setGreedy;
 				
 	// Check each function execution time
-	if (subsetGreedy(inputBenchmark[i].arr, inputBenchmark[i].n, inputBenchmark[i].sum) == true)
+	if (subsetGreedy(inputBenchmark[i].arr, inputBenchmark[i].n, inputBenchmark[i].sum, setGreedy) == true)
       	  foundSetG++;
     	else
       	  notFoundSetG++;
 
-	writeFile(inputBenchmark[i].arr, i, inputBenchmark[i].sum);	
+	//writeFile(inputBenchmark[i].arr, i, inputBenchmark[i].sum);	
 
-    	//auto t2 = high_resolution_clock::now();
+    	auto t2 = high_resolution_clock::now();
 
     	// Getting number of milliseconds as an integer. 
-    	t_ms = duration_cast<seconds>(t2 - t1);
+    	t_ms = duration_cast<microseconds>(t2 - t1);
 		
 			// If the benchmark instance took more than 60s then time limit exceeded
 			if(t_ms.count() < timeLimit){
@@ -228,7 +330,7 @@ int main(){
 				cout<<"Benchmark " << i+1 << " Time limit exceeded\n";
 			}
 
-
+	
 			
 		}
 
@@ -236,6 +338,52 @@ int main(){
 		cout<<"Number of sets not found: " << notFoundSetG <<endl;
 		cout<<"Fraction of benchmarks completed: " << benchmarksCompleteG << " out of " << totalInstances 
 																							 << " Percentage: " << (benchmarksCompleteG*100)/totalInstances <<endl;
+	*/
+	
+	cout<<endl<<"Running Simulated Annealing----------------------------------------"<<endl;
+
+
+	// Run each instance
+	for(int i=0;i<totalInstances;i++){
+	
+    	t1 = high_resolution_clock::now();
+	
+	
+	// Create an initial solution
+	vector<int> setGreedyInitial;
+				
+	//Get the Initial solution set through greedy
+	subsetGreedy(inputBenchmark[i].arr, inputBenchmark[i].n, inputBenchmark[i].sum - (rand()%30 + 1), setGreedyInitial);
+
+	if (simulatedAnnealing(setGreedyInitial, inputBenchmark[i].sum, inputBenchmark[i].arr) == true)
+      	  foundSetS++;
+    	else
+      	  notFoundSetS++;	
+	
+	
+    	//auto t2 = high_resolution_clock::now();
+
+    	// Getting number of milliseconds as an integer. 
+    	t_ms = duration_cast<microseconds>(t2 - t1);
+		
+			// If the benchmark instance took more than 60s then time limit exceeded
+			if(t_ms.count() < timeLimit){
+				benchmarksCompleteS++;
+				cout<<"Benchmark " << i+1 << " completed in time: " << (-1 * t_ms.count())/1000 << "\n\n";
+			}
+			else{
+				cout<<"Benchmark " << i+1 << " Time limit exceeded\n\n";
+			}
+
+	
+			
+	}
+	
+		cout<<"Number of sets found: " << foundSetS <<endl;
+		cout<<"Number of sets not found: " << notFoundSetS <<endl;
+		cout<<"Fraction of benchmarks completed: " << benchmarksCompleteS << " out of " << totalInstances 
+						<< " Percentage: " << (benchmarksCompleteS*100)/totalInstances <<endl;
+	
 
 	return 0;
 }
